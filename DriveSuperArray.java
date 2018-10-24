fe09ad9d1533f833e4e6592706a7d4914c7a76bb
Mohammad Jawwad <mjawwad@stuy.edu>
2018-10-24 19:03:18 -0400

    updated driver to show proof that the 'list' can be lengthened with resize/add as well as print an error message for invalid index

--- DriveSuperArray.java
+++ DriveSuperArray.java
@@ -17,8 +17,8 @@ public class DriveSuperArray{
       System.out.println (l1.toStringDebug()); // [cat, horse, nullx8]
       System.out.println (l1.get(1)); // horse
       System.out.println (l1.get(0)); // cat
-      System.out.println (l1.get(2)); //null
-      System.out.println (l1.set(2, "camel")); //null
+      System.out.println (l1.get(2)); // invalid index error; returning null;
+      System.out.println (l1.set(2, "camel")); //invalid index error; returning null
       System.out.println (l1.set(1, "camel")); //horse
       System.out.println (l1); // [cat, camel]
       l1.clear();
@@ -31,5 +31,19 @@ public class DriveSuperArray{
       System.out.println (l1.isEmpty()); //false
       System.out.println (l1); //[cat]
       System.out.println (l1.toStringDebug()); // [cat, nullx9]
+      l1.add("cow");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      l1.add("pig");
+      System.out.println(l1);// [cat,cow, pigx8]
+      System.out.println(l1.toStringDebug()); // [cat,cow, pigx8]
+      l1.add("new");
+      System.out.println(l1.toStringDebug());// [cat, cow, pigx8, new, nullx9]
+
   }
 }
