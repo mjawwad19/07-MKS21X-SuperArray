5c702f10318b729e3ecd7b1fa302d2cefc02936f
Mohammad Jawwad <mjawwad@stuy.edu>
2018-10-23 20:42:44 -0400

    Reorganized my test order

--- Driver.java
+++ Driver.java
@@ -3,18 +3,17 @@ public class Driver{
       SuperArray l1 = new SuperArray();
       System.out.println (l1.size()); //0
       System.out.println (l1.isEmpty()); //true
-      System.out.println (l1.toStringDebug()); // [10 null array]
       System.out.println (l1); //[]
+      System.out.println (l1.toStringDebug()); // [10 null array]
       l1.add("cat");
       System.out.println (l1.size()); //1
       System.out.println (l1.isEmpty()); //false
+      System.out.println (l1); //[]
       System.out.println (l1.toStringDebug()); // [cat, nullx9]
-      System.out.println (l1); // [cat]
       l1.add("horse");
       System.out.println (l1.size()); //2
       System.out.println (l1.isEmpty()); //false
+      System.out.println (l1); //[]
       System.out.println (l1.toStringDebug()); // [cat, horse, nullx8]
-      System.out.println (l1); // [cat, horse]
   }
 }
-
